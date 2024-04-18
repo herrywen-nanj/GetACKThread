394f71cde659bd82ffe42850f99f05381de32849
herrywen1 <13585162465@163.com>
2024-04-18 22:28:22 +0800

    update check_times_counter_controller.sh

--- main.sh
+++ main.sh
@@ -14,8 +14,7 @@
 # 命名空间  
 NAMESPACE="default" 
   
-# CPU 使用率阈值  
-THRESHOLD=90  
+  
   
 # 获取命名空间中所有 Pod 的 CPU 使用率  
 PODS=$(kubectl  --kubeconfig  $kubeconfig  get pods -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')  
@@ -33,34 +32,26 @@ for POD in $PODS; do
 	# 查询pod是否就绪,限制条件只适合pod内只有一个容器
 	STATUS_PHASE=$(kubectl  --kubeconfig  $kubeconfig get pods  -o=jsonpath="{.items[?(@.metadata.name == '$POD')].status.phase}")
 	STATUS_READY=$(kubectl  --kubeconfig  $kubeconfig get pods -o=jsonpath="{.items[?(@.metadata.name == '$POD')].status.containerStatuses}"| sed "s/[][]//g" | jq ".ready")
-	if [[ $STATUS_PHASE == "Running" ]] && [[ $STATUS_READY == "true" ]];then 
-		# 从 Prometheus 查询 Pod 的 CPU 使用率  
-		CPU_USAGE=`curl  -H "Authorization: Bearer $TOKEN" -s  "$PROMETHEUS_URL/api/v1/query_range?query=sum(irate(container_cpu_usage_seconds_total\{pod=~\"$POD\",namespace=\"$NAMESPACE\"\}\[1m\]))by(pod)&start=$(date -d '1 minute ago' +%s)&end=$(date +%s)&step=10"| jq -r '.data.result[].values[-1][1]'`
-		[ -z $CPU_USAGE ] && continue
-		CPU_USAGE_TOTAL=`curl -s  "$PROMETHEUS_URL/api/v1/query_range?query=max(container_spec_cpu_quota\{pod=~\"$POD\",namespace=\"$NAMESPACE\"\})&start=$(date -d '1 minute ago' +%s)&end=$(date +%s)&step=61" | jq -r '.data.result[].values[-1][1]'`
-		[ -z $CPU_USAGE_TOTAL ] && continue
-		CPU_USAGE_PERCENT=`echo "${CPU_USAGE} *1000*100*100/$CPU_USAGE_TOTAL" |bc `
-		echo $CPU_USAGE_PERCENT
-		  
-		# 检查 CPU 使用率是否超过阈值,需要提前设置ACK中应用的StartupProbe探针的initialDelaySeconds参数为3m即设置180s，启动完成程序会占用较高CPU，运行大概3m会趋近正常  
-		if [[ "$CPU_USAGE_PERCENT" -gt "$THRESHOLD" ]]; then
-		Check_Times_Counter
-			echo "$(date +"%Y-%m-%d-%H:%M")    Pod $POD CPU使用率为$CPU_USAGE_PERCENT超过 $THRESHOLD%，正在重启 Pod"   >> $(date +%F)_delete_pod.txt
-			kubectl  --kubeconfig  $kubeconfig exec -it $POD -- sh<dump_jvm.sh > /dev/null 2>&1
-			if [[ $? -eq 0 ]]; then
-			# 拷贝dump文件
-				kubectl  --kubeconfig  $kubeconfig exec -it $POD -- ls>$POD.txt
-				DUMPFILENAME=$(cat $POD.txt | grep "$POD"|awk '{print $1}')
-				TIMESTAMP=`date "+%Y-%m-%d-%H-%M-%S"`
-				kubectl  --kubeconfig  $kubeconfig cp $POD:$DUMPFILENAME $DUMPPATH$POD$TIMESTAMP.hrpof
-			fi
-			echo "Pod $POD CPU 使用率超过 $THRESHOLD%，正在重启 Pod"  
-			# 删除 Pod，Kubernetes 会根据 Replicaset 或 Deployment 自动重启		 
-			kubectl  --kubeconfig $kubeconfig  delete pod $POD --namespace $NAMESPACE 
-			sleep 10
-		# 上传到ossbucket
-		#[ -e "${DUMPPATH}/${DUMPFILENAME}" ] && ossutil64 cp -r -u "${DUMPPATH}/${DUMPFILENAME}" ${OSS_PATH} || echo "Can't find ${DUMPFILENAME} in ${DUMPPATH}!"
+	if [[ $STATUS_PHASE == "Running" ]] && [[ $STATUS_READY == "true" ]];then
+		# 执行检查控制器 
+		Check_Times_Counter_Controller
+		# 日志写入		  
+		echo "$(date +"%Y-%m-%d-%H:%M")    Pod $POD CPU使用率为$CPU_USAGE_PERCENT超过 $THRESHOLD%，正在重启 Pod"   >> $(date +%F)_delete_pod.txt
+		# 执行线程dump过程
+		kubectl  --kubeconfig  $kubeconfig exec -it $POD -- sh<dump_jvm.sh > /dev/null 2>&1
+		if [[ $? -eq 0 ]]; then
+		# 拷贝dump文件
+			kubectl  --kubeconfig  $kubeconfig exec -it $POD -- ls>$POD.txt
+			DUMPFILENAME=$(cat $POD.txt | grep "$POD"|awk '{print $1}')
+			TIMESTAMP=`date "+%Y-%m-%d-%H-%M-%S"`
+			kubectl  --kubeconfig  $kubeconfig cp $POD:$DUMPFILENAME $DUMPPATH$POD$TIMESTAMP.hrpof
 		fi
+		echo "Pod $POD CPU 使用率超过 $THRESHOLD%，正在重启 Pod"  
+		# 删除 Pod，Kubernetes 会根据 Replicaset 或 Deployment 自动重启		 
+		kubectl  --kubeconfig $kubeconfig  delete pod $POD --namespace $NAMESPACE 
+		sleep 10
+		# 上传到ossbucket
+		#[ -e "${DUMPPATH}/${DUMPFILENAME}" ] && ossutil64 cp -r -u "${DUMPPATH}/${DUMPFILENAME}" ${OSS_PATH} || echo "Can't find ${DUMPFILENAME} in ${DUMPPATH}!"			
 	fi
 done
 
